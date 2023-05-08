6d2539216db2b9d415b644f811b3fda0c9862776
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-05-08 17:29:44 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,54 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const name = form.name.value;
+    console.log(email, name);
+
+    const user = { name, email };
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        form.reset()
+      });
+  };
 
   return (
     <>
-  
-        
       <h1>User management System</h1>
-     
+      {users.length}
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="add" />
+      </form>
+
+
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
